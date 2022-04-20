07db4507e2778f439110901ba5ee11d45d6d348a
Kcee42 <chilakakelechi85@gmail.com>
2022-04-19 17:08:35 -0700

    commit k

---
+++ _printf.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _printf - format output conversion and print data
+ * @format: input string
+ *
+ * Return: number of chars printed
+ */
+
+int _printf(const char *format, ...)
+{
+	unsigned int i = 0, len = 0; ibuf = 0;
+	va_list arguments;
+	int (*function)(va_list, char *, unsigned int);
+	char *buffer;
